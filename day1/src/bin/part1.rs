82a9b6acbf04be347d4ae1afc0b0b1ace2fe2b2c
HenningCode <henningwilmer@yahoo.de>
2023-12-03 16:42:14 +0100

    optimazations to Par1

--- day1/src/bin/part1.rs
+++ day1/src/bin/part1.rs
@@ -1,41 +1,31 @@
 use std::fs;
-use std::num::ParseIntError;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Not a valid file");
-    let result = parse_input(input);
+    let result = parse_input(&input);
     println!("{result}");
 }
 
-fn try_parse_to_int(str: String) -> Result<i32, ParseIntError> {
-    let parse = str.parse()?;
-    Ok(parse)
-}
-
-fn parse_input(input: String) -> i32 {
-    let lines: Vec<String> = input.lines().map(|x| String::from(x)).collect();
+fn parse_input(input: &str) -> i32 {
     let mut result = 0;
 
-    for x in lines {
-        let char_vec: Vec<char> = x.chars().collect();
+    for x in input.lines() {
         let mut int_vec: Vec<i32> = Vec::new();
-        for char in char_vec {
-            let string_char = char.to_string();
-            if let Ok(try_parse) = try_parse_to_int(string_char) {
-                int_vec.push(try_parse);
+        for char in x.chars() {
+            if char.is_digit(10){
+                int_vec.push(char.to_string().parse().unwrap());
             }
         }
         let mut together_int: i32 = 0;
         if let Some(result) = int_vec.first() {
-            let value1 = result.to_string().to_owned();
-            let value2: String;
+            let value1 = result;
+            let value2:i32;
             if let Some(result2) = int_vec.last() {
-                value2 = result2.to_string().to_owned();
+                value2 = *result2;
             } else {
-                value2 = value1.clone().to_owned();
+                value2 =*value1;
             }
-            let together = format!("{value1}{value2}");
-            together_int = together.parse().unwrap();
+            together_int = value1 * 10 + value2;
         }
         result += together_int;
     }
@@ -47,7 +37,7 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet".to_string();
+        let input = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
         assert_eq!(parse_input(input), 142);
     }
 }
