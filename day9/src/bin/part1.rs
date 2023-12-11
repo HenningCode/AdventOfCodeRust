89cb88c70170276b44a3195f96c45218343751ca
HenningCode <henningwilmer@yahoo.de>
2023-12-11 17:52:29 +0100

    Part1 Day9

--- day9/src/bin/part1.rs
+++ day9/src/bin/part1.rs
@@ -1,1 +1,82 @@
-fn main() {}
+use std::fs;
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Not a valid file");
+    let result = parse_input(&input);
+    println!("{result}");
+}
+
+fn get_number_from_line(input: &str) -> Vec<i64> {
+    let mut number_vec = Vec::new();
+
+    for number in input.trim().split(' ') {
+        if let Ok(number) = number.parse() {
+            number_vec.push(number)
+        };
+    }
+    number_vec
+}
+
+fn calc_rows(input: Vec<i64>) -> Vec<Vec<i64>> {
+    let mut row_vec: Vec<Vec<i64>> = vec![input];
+    loop {
+        let current_row = row_vec.last().unwrap();
+        let mut new_line = Vec::new();
+        for i in 0..current_row.len() - 1 {
+            new_line.push(current_row[i + 1] - current_row[i])
+        }
+        row_vec.push(new_line);
+        if row_vec.last().unwrap().iter().sum::<i64>() == 0 {
+            break;
+        }
+    }
+    row_vec
+}
+
+fn calc_continuation(row_matix: &mut Vec<Vec<i64>>) -> i64 {
+    let mut first_row = true;
+    let mut last_row: &mut Vec<i64> = &mut Vec::new();
+    let mut result = 0;
+    
+    for (i, row) in &mut row_matix.iter_mut().enumerate().rev() {
+        if first_row {
+            first_row = false;
+            last_row = row;
+            continue;
+        }
+
+        if i == 0 {
+            result = last_row.last().unwrap() + row.last().unwrap();
+        } else {
+            (*row).push(last_row.last().unwrap() + row.last().unwrap());
+            last_row = row;
+        }
+    }
+    result
+}
+
+fn parse_input(input: &str) -> i64 {
+    let mut result = 0;
+
+    for line in input.lines() {
+        let nums = get_number_from_line(line);
+        let mut rows = calc_rows(nums);
+        result += calc_continuation(&mut rows);
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+
+        assert_eq!(parse_input(input), 114);
+    }
+}
