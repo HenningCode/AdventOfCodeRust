a5f5e8a43fb63984b6585f4a42988c89ac1f6c11
HenningCode <henningwilmer@yahoo.de>
2023-12-11 18:56:48 +0100

    Init Day10

---
+++ day10/src/bin/part1.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Not a valid file");
+    let result = parse_input(&input);
+    println!("{result}");
+}
+
+fn parse_input(input: &str) -> u32{
+
+    4
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_input(){
+        let input = "";
+        assert_eq!(parse_input(input), 288)
+    }
+}
